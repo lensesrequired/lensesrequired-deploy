44527bb941cb10bce4c0b91cdb11855fc0da2e91
Anna <admin@lensesrequired.com>
2025-11-30 18:11:32 -0700

    Add Courtograph app stack

--- bin/lensesrequired-deploy.ts
+++ bin/lensesrequired-deploy.ts
@@ -2,11 +2,15 @@
 import * as cdk from 'aws-cdk-lib';
 import { MovieNightStack } from '../lib/movie-night-stack';
 import { JournalStack } from "../lib/journal-stack";
+import {CourtographStack} from "../lib/courtograph-stack";
 
 const app = new cdk.App();
 new MovieNightStack(app, 'MovieNightStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
 new JournalStack(app, 'JournalStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
+});
+new CourtographStack(app, 'CourtographStack', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION }
 });
---
+++ lib/courtograph-stack.ts
@@ -0,0 +1,76 @@
+import * as cdk from 'aws-cdk-lib';
+import {SecretValue} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import {ProjectionType} from 'aws-cdk-lib/aws-dynamodb';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
+
+export class CourtographStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const table = new dynamodb.TableV2(this, 'CourtoggraphGamesTable', {
+      tableName: 'courtograph-games-table',
+      billing: dynamodb.Billing.onDemand({
+        maxReadRequestUnits: 50,
+        maxWriteRequestUnits: 25
+      }),
+      partitionKey: { name: 'gameId', type: dynamodb.AttributeType.STRING },
+      deletionProtection: true,
+      timeToLiveAttribute: 'ttl',
+      removalPolicy: cdk.RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE,
+      pointInTimeRecoverySpecification: {
+        pointInTimeRecoveryEnabled: true,
+      }
+    });
+
+    const execUser = new iam.User(this, 'CourtoggraphUser', {
+      userName: 'courtograph-exec-user'
+    })
+    execUser.attachInlinePolicy(new iam.Policy(this, 'CourtoggraphExecPolicy', {
+      statements: [
+        new iam.PolicyStatement({
+          sid: 'DynamoDBReadAccess',
+          actions: [
+            "dynamodb:List*",
+            "dynamodb:DescribeReservedCapacity*",
+            "dynamodb:DescribeLimits",
+            "dynamodb:DescribeTimeToLive"
+          ],
+          resources: ['*']
+        }),
+        new iam.PolicyStatement({
+          sid: 'DynamoDBWriteAccess',
+          actions: [
+            "dynamodb:BatchGet*",
+            "dynamodb:DescribeStream",
+            "dynamodb:DescribeTable",
+            "dynamodb:Get*",
+            "dynamodb:Query",
+            "dynamodb:Scan",
+            "dynamodb:BatchWrite*",
+            "dynamodb:CreateTable",
+            "dynamodb:Delete*",
+            "dynamodb:Update*",
+            "dynamodb:PutItem"
+          ],
+          resources: [table.tableArn, `${table.tableArn}/*`]
+        })
+      ]
+    }))
+    execUser.node.addDependency(table)
+
+    const accessKey = new iam.AccessKey(this, 'CourtographExecUserAccessKey', { user: execUser });
+    accessKey.node.addDependency(execUser);
+
+    const secret = new secretsmanager.Secret(this, 'CourtographAccessKeySecret', {
+      secretObjectValue: {
+        Username: SecretValue.unsafePlainText(execUser.userName),
+        AccessKeyId: SecretValue.unsafePlainText(accessKey.accessKeyId),
+        SecretAccessKey: accessKey.secretAccessKey
+      },
+    });
+    secret.node.addDependency(accessKey)
+  }
+}
